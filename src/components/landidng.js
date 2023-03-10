3ea5f00b3b0e475e30f39cb8d4d01a5a876c345e
kaleab-taye <kaleabtaye@gmail.com>
2023-03-11 01:13:31 +0300

    assets added

---
+++ src/components/landidng.js
@@ -0,0 +1,12 @@
+export default function LandingComponet() {
+    return <>
+        <div className=' grid h-[100vh]' >
+            <div class="min-w-full bg-[url('/images/Asset_14.png')] bg-cover bg-center">
+                <div class="w-full h-full grid  justify-center backdrop-brightness-90">
+                    <div class="text-white text-4xl  mt-auto text-center">Studio Stunnit</div>
+                    <div class="text-white text-xl text-center">Project Under Development</div>
+                </div>
+            </div>
+        </div>
+    </>
+}
