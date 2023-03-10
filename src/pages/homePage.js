14265de1a6339fb50d655a04ec9dc8bb59eb9027
kaleab-taye <kaleabtaye@gmail.com>
2023-03-10 04:22:16 +0300

    project under development

---
+++ src/pages/homePage.js
@@ -0,0 +1,41 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import cover_1 from '../../public/images/Cover-1.webp'
+
+export default function HomePage() {
+    return <>
+    <Head>
+      <title>Studio Stunnit</title>
+      <meta name="description" content="We help you step into stunning Interior design spaces that is
+customized to ﬁt your personal value. Get in touch and let’s
+start customizing a design that ﬁts just right for you!" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <main className=' grid h-[100vh]' >
+        {/* <div className="bg-cover bg-[url('/images/Cover-1.webp')] h-fit"> */}
+
+        {/* <div className='border-2 border-green-400 grid'>
+        <Image src={cover_1} className='min-w-[100vw]'/>
+                <div className='fixed w-[100%] h-[100%]'>
+        <div className='grid m-auto bg-red-400'>
+<div>
+
+                    Studio Stunnit</div>
+</div>
+        </div>
+            
+        </div> */}
+
+        <div
+        class="min-w-full bg-[url('/images/Cover-1.webp')] bg-cover bg-center">
+        <div class="w-full h-full grid  justify-center backdrop-brightness-50">
+        <div class="text-white text-4xl  mt-auto text-center">Studio Stunnit</div>
+        <div class="text-white text-xl text-center">Project Under Development</div>
+        </div>
+    </div>
+        {/* </div> */}
+    </main>
+    </>
+  }
+  
