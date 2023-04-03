96a6e729cc6bc9cb6549a5e2b2c350f9f2db7ecd
Estifanos Neway <Estifanos.Neway.D@gmail.com>
2023-04-03 11:23:23 +0300

    fixing admin signin

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,9 +3,6 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
-    console.log("-------------------",!request.url.endsWith("/signin"))
-    console.log("-------------------",!request.url.endsWith("/signin") && request.cookies.get('signed-in')?.value !== 'true')
-    console.log(request.cookies.get('signed-in')?.value)
     if (!request.url.endsWith("/signin") && request.cookies.get('signed-in')?.value !== 'true') {
         return NextResponse.redirect(new URL('/admin/signin', request.url))
     }
